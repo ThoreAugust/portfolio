8ad78ab23ebc62ff2cf995dda0794bfb03797c8e
ThoreAugust <32572122+ThoreAugust@users.noreply.github.com>
2020-08-26 22:35:15 +0200

    first part of great gatsby bootcamp

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,20 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/`
+      }
+    },
+    'gatsby-transformer-remark'
+  ],
+  siteMetadata:{
+    title: 'Thore August - Full-Stack Dev in training',
+    author: 'Thore August',
+    published: '2020'
+  }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
-import React from "react"
+import React from 'react';
+import {Link} from 'gatsby'
+import Layout from '../components/layout';
 
-export default function Home() {
-  return <div>Hello world!</div>
+const Home = () =>{
+  return(
+    <Layout>
+      <h1>Hello,</h1>
+      <h2>I'm Thore, a full-stack developer living in Hamburg (Germany).</h2>
+      <p>Need a developer? <Link to="/contact">Contact me.</Link></p>
+    </Layout>
+  )
 }
+
+export default Home;
