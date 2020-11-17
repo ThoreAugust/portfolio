8b75a4cd4898067bcf197d3bd50320f2b380b748
ThoreAugust <32572122+ThoreAugust@users.noreply.github.com>
2020-11-17 23:17:15 +0100

    removed recaptcha for testing

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -5,13 +5,13 @@ const ContactForm = () =>{
     return(
         <div className={contactFormStyle.container}>
             <div className={contactFormStyle.formLeft}>
-                <form name="contact" data-netlify="true" data-netlify-recaptcha="true" method="POST">
+                <form name="contact" data-netlify="true" method="POST">
                     <p>Leave a message</p>
                     <input type="text" placeholder="Name*" tabIndex="0"/>
                     <input type="email" placeholder="E-Mail*" tabIndex="0"/>
                     <input type="text" placeholder="Subject*" tabIndex="0"/>
                     <textarea placeholder="Message*" tabIndex="0"/>
-                    <div data-netlify-recaptcha="true" className={contactFormStyle.recaptcha}></div>
+
                     <button type="submit" tabIndex="0">Get in touch!</button>
                 </form>
             </div>
