823fec0e030a1236f15a18ab846fbd2a676598be
ThoreAugust <32572122+ThoreAugust@users.noreply.github.com>
2020-11-17 23:28:39 +0100

    more testing sofar not working

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -5,14 +5,14 @@ const ContactForm = () =>{
     return(
         <div className={contactFormStyle.container}>
             <div className={contactFormStyle.formLeft}>
-                <form name="contact" data-netlify="true" method="POST">
+                <form name="contact" method="POST" data-netlify="true">
                     <p>Leave a message</p>
-                    <input type="text" placeholder="Name*" name="name" tabIndex="0"/>
-                    <input type="email" placeholder="E-Mail*" name="email" tabIndex="0"/>
-                    <input type="text" placeholder="Subject*" name="subject" tabIndex="0"/>
-                    <textarea placeholder="Message*" name="message" tabIndex="0"/>
+                    <input type="text" placeholder="Name*" name="name" />
+                    <input type="email" placeholder="E-Mail*" name="email" />
+                    <input type="text" placeholder="Subject*" name="subject" />
+                    <textarea placeholder="Message*" name="message" />
 
-                    <button type="submit" tabIndex="0">Get in touch!</button>
+                    <button type="submit">Get in touch!</button>
                 </form>
             </div>
             <div className={contactFormStyle.detailsRight}>
