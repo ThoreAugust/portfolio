f2bef7ab5212168bb0bdbffc553e3de46851b3f9
ThoreAugust <32572122+ThoreAugust@users.noreply.github.com>
2020-11-17 23:20:59 +0100

    added name attributes to inputfields

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -7,10 +7,10 @@ const ContactForm = () =>{
             <div className={contactFormStyle.formLeft}>
                 <form name="contact" data-netlify="true" method="POST">
                     <p>Leave a message</p>
-                    <input type="text" placeholder="Name*" tabIndex="0"/>
-                    <input type="email" placeholder="E-Mail*" tabIndex="0"/>
-                    <input type="text" placeholder="Subject*" tabIndex="0"/>
-                    <textarea placeholder="Message*" tabIndex="0"/>
+                    <input type="text" placeholder="Name*" name="name" tabIndex="0"/>
+                    <input type="email" placeholder="E-Mail*" name="email" tabIndex="0"/>
+                    <input type="text" placeholder="Subject*" name="subject" tabIndex="0"/>
+                    <textarea placeholder="Message*" name="message" tabIndex="0"/>
 
                     <button type="submit" tabIndex="0">Get in touch!</button>
                 </form>
