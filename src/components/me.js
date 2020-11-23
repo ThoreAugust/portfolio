205227beb36bba6fa19a7e84ec12f2dd8001e82c
ThoreAugust <32572122+ThoreAugust@users.noreply.github.com>
2020-11-23 21:34:42 +0100

    Added Text to me section

--- src/components/me.js
+++ src/components/me.js
@@ -3,6 +3,15 @@ import me from '../../static/200902_Thore_portrait.png'
 import aboutStyle from './me.module.scss'
 
 const Me = () =>{
+    const seventeenthBD = new Date('December 3, 2008');
+    const today = new Date();
+    const getYearDiff = (date1, date2)=>{
+        let diffYears = date1.getFullYear() - date2.getFullYear();
+        let diffMonths = date2.getMonth() - date1.getMonth();
+        if(diffMonths >= 6) return diffYears - 1;
+        return diffYears;
+    }
+    const diff = getYearDiff(today, seventeenthBD);
     return(
         <div className={aboutStyle.container}>
             <div className={aboutStyle.leftImg}>
@@ -11,7 +20,11 @@ const Me = () =>{
             <div className={aboutStyle.message}>
                 <h1>Hi, I am Thore August,<br/> Developer from Germany.</h1>
                 <p>
-                    Hier kommt noch Text hin.
+                    I found my passion for programming about {diff} years ago during high school but did not commit to a career as a developer until 2016. 
+                    I moved to Hamburg, Germany to pursue my bachelor’s degree in media systems at the HAW Hamburg which I started the following spring.<br />
+                    During my studies I created some small projects with my peers which you can find on my github.<br />
+                    I got my first and current developer job at Workplace Solutions GmbH in summer 2019 during my fifth semester. <br/>
+                    Currently I am starting a blog about working as a developer struggling with media addiction.
                 </p>
                 <a href="/contact#contactForm">Get in touch!</a>
             </div>
